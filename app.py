fc9800ab25dca253dbabaf1cdea0c595beae52d1
Yogesh Gahlot <yogeshgahlot100@gmail.com>
2025-03-15 15:39:46 +0530

    updating camera

--- app.py
+++ app.py
@@ -89,8 +89,7 @@ def generate_frames():
             _, buffer = cv.imencode('.jpg', image)
             frame = buffer.tobytes()
 
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
